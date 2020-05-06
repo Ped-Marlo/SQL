14ad75af535035ec8b8495d09aa2a6ce34420cda
Pedro Martinez Lopez <pedroml5@icloud.com>
2020-05-06 11:53:10 +0200

    Initial commit

---
+++ ejercicios_practica/Inserts.sql
@@ -0,0 +1,29 @@
+ INSERT INTO cliente VALUES ( '12345', 'Luis Díaz', 'Santa Rita No. 18' );
+ INSERT INTO cliente VALUES ( '12344', 'Javier Lozano', '11 Sur No.35' );
+ INSERT INTO cliente VALUES ( '12343', 'Andres López', 'San Agustin No.81' );
+ INSERT INTO cliente VALUES ( '12346', 'Alondra López', 'Santa Rita No.1501' );
+
+ INSERT INTO cuenta VALUES (  '6789',  'Dorada', 120.01, 3.1, '2011-03-12' );
+ INSERT INTO cuenta VALUES (  '6788',  'Dorada', 23445.01, 3.1, '2011-03-12' );
+ INSERT INTO cuenta VALUES (  '6787',  'Dorada', 4566.78, 3.1, '2011-03-12' );
+ INSERT INTO cuenta VALUES (  '6786',  'Angelópolis', 1222.3, 3.1, '2011-03-12' );
+ 
+ INSERT INTO impositor VALUES ( '12345', '6789' );
+ INSERT INTO impositor VALUES ( '12345', '6786' );
+ INSERT INTO impositor VALUES ( '12344', '6788' );
+ INSERT INTO impositor VALUES ( '12343', '6787' );
+
+
+ insert into sucursal values ('Guadalajara 1', 'Guadalajara', 123454321.09);
+ insert into sucursal values ('Guadalajara 2', 'Guadalajara', 824754391.91);
+ insert into sucursal values ('Guadalajara 3', 'Guadalajara', 759373484.96);
+ insert into sucursal values ('Madrid 1', 'Madrid', 123464564.87);
+ insert into sucursal values ('Madrid 2', 'Madrid', 645646421.13);
+ 
+ insert into empleados values ('e12345', 'Pepito Díaz','1222299988','Calle 13 Norte No. 5','Madrid',12343.90);
+ insert into empleados values ('e12344', 'Juan Méndez','1342794996','Calle 41 Norte No. 612','Madrid',12343.90);
+ insert into empleados values ('e12343', 'Maria Ortega','1322693985','Calle 67 Poniente No. 91','Guadalajara',12343.90);
+ insert into empleados values ('e12342', 'Jesús Robles','1422592976','Calle 90 Oriente No. 543','Guadalajara',12343.90);
+ insert into empleados values ('e12341', 'Ricardo Vargas','1522491967','Calle 11 Sur No. 6780','Madrid',12343.90);
+ 
+ 
